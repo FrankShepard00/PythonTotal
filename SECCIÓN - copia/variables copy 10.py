83f23055e5c4e4c21b92b62d6bec3e512280bfde
FrankShepard00 <136857218+FrankShepard00@users.noreply.github.com>
2025-05-20 03:33:31 -0600

    Operadores y formatos

---
+++ SECCIÓN - copia/variables copy 10.py
@@ -0,0 +1,7 @@
+edad = input("Dime tu edad: ")
+print("Tu edad es: " + edad)
+print(type(edad))
+print(edad + edad)
+
+nueva_edad = 1 + edad
+print("Vas a cumplir " + nueva_edad)
---
+++ SECCIÓN - copia/variables copy 9.py
@@ -0,0 +1,3 @@
+mi_numero =  5 + 5.8
+print(mi_numero)
+print(type(mi_numero))
